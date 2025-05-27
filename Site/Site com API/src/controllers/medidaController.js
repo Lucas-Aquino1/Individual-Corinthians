651e5179c8116d6ea758d31f737b62cb6aee0621
Lucas-Aquino1 <lucas.aquino@sptech.school>
2025-05-27 12:57:58 -0300

    Começando a Dashboard, fazendo CSS e Rotas para consulta

--- Site/Site com API/src/controllers/medidaController.js
+++ Site/Site com API/src/controllers/medidaController.js
@@ -2,13 +2,13 @@ var medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
 
-    const limite_linhas = 7;
+    const limite_linhas = 5;
 
-    var idAquario = req.params.idAquario;
+    var idGrafico = req.params.idGrafico;
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(idGrafico, limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -24,11 +24,11 @@ function buscarUltimasMedidas(req, res) {
 
 function buscarMedidasEmTempoReal(req, res) {
 
-    var idAquario = req.params.idAquario;
+    var idIndicador = req.params.idIndicador;
 
     console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
+    medidaModel.buscarMedidasEmTempoReal(idIndicador).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
--- Site/Site com API/src/models/medidaModel.js
+++ Site/Site com API/src/models/medidaModel.js
@@ -1,32 +1,129 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+// SELECT PARA OS GRAFICOS
+function buscarUltimasMedidas(idGrafico, limite_linhas) {
+    if (idGrafico == 1) {
+        var instrucaoSql = `
+        SELECT socioTorcedor, COUNT(*) AS total
+        FROM usuario
+        GROUP BY socioTorcedor;
+        `;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+    if (idGrafico == 2) {
+        var instrucaoSql = `
+        SELECT u.nome, r.acertos
+        FROM resultadosQuiz r
+        JOIN usuario u ON r.idUsuario = u.id
+        ORDER BY r.acertos DESC
+        LIMIT 5;
+        `;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
+
+    if (idGrafico == 3) {
+        var instrucaoSql = `
+        SELECT u.nome, r.porcentagemFinalDeAcertos
+        FROM resultadosQuiz r
+        JOIN usuario u ON r.idUsuario = u.id;
+    `;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
+
+    if (idGrafico == 4) {
+        var instrucaoSql = `
+        SELECT jogadorPreferido, COUNT(*) AS total
+        FROM usuario
+        GROUP BY jogadorPreferido
+        ORDER BY total DESC
+        LIMIT 5;
+        `;
+
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
+
+    // if (idGrafico == 5) {
+    //     var instrucaoSql = `SELECT jog.apelido as apelido, count(*) as contagem
+    //     FROM usuario usu
+    //     INNER JOIN jogador jog on jog.idjogador = usu.fk_jogadorFavorito
+    //     GROUP BY apelido
+    // ORDER BY contagem DESC LIMIT ${limite_linhas}`;
+
+    //     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    //     return database.executar(instrucaoSql);
+    // }
+
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+// SELECTS PARA AS KPIS
+function buscarMedidasEmTempoReal(idIndicador) {
+    if (idIndicador == 1) {
+        var instrucaoSql = `
+        SELECT ROUND(AVG(porcentagemFinalDeAcertos), 2) AS mediaPontuacao
+        FROM resultadosQuiz;
+        `;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
+
+    if (idIndicador == 2) {
+        var instrucaoSql = `
+        SELECT jogadorPreferido, COUNT(*) AS total
+        FROM usuario
+        GROUP BY jogadorPreferido
+        ORDER BY total DESC
+        LIMIT 1;
+        `;
+
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
+
+    if (idIndicador == 3) {
+        var instrucaoSql = `
+        SELECT 
+        ROUND(100.0 * SUM(CASE WHEN socioTorcedor = 'S' THEN 1 ELSE 0 END) / COUNT(*), 2) AS porcentagemSocios
+        FROM usuario;
+        `;
+
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
+
+    // if (idIndicador == 4) {
+    //     var instrucaoSql = `SELECT faixa_idade
+    //     FROM view_faixaIdade
+    //     WHERE contagem = (SELECT MAX(contagem) FROM view_faixaIdade)`;
+
+    //     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    //     return database.executar(instrucaoSql);
+    // }
+
+    // if (idIndicador == 5) {
+    //     var instrucaoSql = `SELECT jogador
+    //     FROM view_jogador
+    //     WHERE contagem = (SELECT MAX(contagem) FROM view_jogador)`;
+
+    //     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    //     return database.executar(instrucaoSql);
+    // }
+
+    // if (idIndicador == 6) {
+    //     var instrucaoSql = `SELECT regiao
+    //     FROM view_regiao
+    //     WHERE contagem = (SELECT MAX(contagem) FROM view_regiao)`;
+
+    //     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    //     return database.executar(instrucaoSql);
+    // }
 }
 
 module.exports = {
